7bf5169607a8b652b5e361e5fe55a7f53c229229
Ayushi Jain <97507160+ayushi8855@users.noreply.github.com>
2023-03-31 17:54:14 +0530

    Add files via upload

---
+++ src/component/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+        <div style={{width:"500px", height:"200px", margin:"auto",marginTop:"200px"}}>
+            <input type="text" />
+            <button>Search</button>
+        </div>
+    </div>
+  )
+}
+
+export default Search
